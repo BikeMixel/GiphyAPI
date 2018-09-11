8f4bee38e608bf82fccf94740507c4130ae67aeb
BikeMixel <bixelmike@gmail.com>
2018-09-10 21:28:55 -0400

    Almost done

---
+++ assets/javascript/app.js
@@ -0,0 +1,56 @@
+
+var topics = ["football", "baseball", "basketball", "hockey"]
+
+function renderPage() {
+    for (var i = 0; i < topics.length; i++) {
+        $("#buttons").append("<button sport-name='" + topics[i] + "'>" + topics[i] + "</button>")
+    }
+    $("button").on("click", function () {
+        $("#imageHolder").empty()
+
+        var sportName = $(this).attr("sport-name")
+        var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + sportName +
+            "&api_key=X6XVEDUzCINKWL1WN2aTAwdXkBJVxn2j"
+
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            var results = response.data;
+            for (var j = 0; j < 10; j++) {
+                var imageDiv = $("#imageHolder")
+                var p = $("<p>").text("Rating: " + results[i].rating)
+                var sportImage = $("<img src='" + results[j].images.fixed_height_still.url + "' source-move='" + results[j].images.fixed_height.url + 
+                                    "' source-still='" + results[j].images.fixed_height_still.url + "' img-state='still'>")
+                sportImage.addClass("gif")
+                imageDiv.prepend(p)
+                imageDiv.prepend(sportImage)
+            }
+            $("img").on("click", function () {
+                var state = $(this).attr("img-state")
+                console.log($(this).attr("img-state"))
+
+                if (state = "still") {
+                    $(this).attr("src", $(this).attr("source-move"))
+                    $(this).attr("img-state", "animate")
+                }
+                else if (state = "animate") {
+                    $(this).attr("src", $(this).attr("source-still"))
+                    $(this).attr("img-state", "still")
+                }
+
+            })
+        })
+    })
+}
+
+$("#add-sport").on("click", function (event) {
+    event.preventDefault();
+    var newSport = $("#newInput").val().trim();
+    topics.push(newSport);
+    console.log(topics)
+    $("#buttons").empty()
+    renderPage();
+})
+renderPage();
